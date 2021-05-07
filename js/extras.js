6fdcee4846ad55027a0bf129116382351e57c23b
akashdecoder <akash.ranjan1999@gmail.com>
2021-05-07 13:53:35 +0530

    Updated hackathon page

--- js/extras.js
+++ js/extras.js
@@ -1,10 +1,15 @@
 function insertValue() {
     if(document.getElementById("branch").value == "OTHERS") {
         var branch = document.getElementById("branch");
-        var promt = prompt("Please Enter branch", "Other Branch");
-        var newOption = document.createElement("OPTION");
-        var newOptionVal = document.createTextNode(promt);
-        newOption.appendChild(newOptionVal);
-        branch.insertBefore(newOption, branch.firstChild);
+        var promt = prompt("Please Enter branch (After Entering revisit the dropdown)");
+        if(promt == "") {
+            alert("Enter value");
+        } else {
+            var newOption = document.createElement("OPTION");
+            var newOptionVal = document.createTextNode(promt);
+            branch.remove(branch.selectedIndex)
+            newOption.appendChild(newOptionVal);
+            branch.insertBefore(newOption, branch.firstChild);
+        }   
     }
 }
